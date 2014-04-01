566785bd2dc4632edbe33c827a06b654e360ace7
Patrick Spieler <stapelzeiger@gmail.com>
2014-04-01 22:46:16 +0200

    add working pid parameters

--- control.c
+++ control.c
@@ -140,19 +140,19 @@ void holonomic_base_speed_cs_init(void)
 {
     // velocity in x
     pid_init(&nastya_cs.vx_pid);
-    pid_set_gains(&nastya_cs.vx_pid, 1, 0, 0); // KP, KI, KD
+    pid_set_gains(&nastya_cs.vx_pid, 20, 4, 1); // KP, KI, KD
     pid_set_maximums(&nastya_cs.vx_pid, VX_MAX_INPUT, 1024, 0); // in , integral, out
     pid_set_out_shift(&nastya_cs.vx_pid, 0);
     pid_set_derivate_filter(&nastya_cs.vx_pid, 3);
     // velocity in y
     pid_init(&nastya_cs.vy_pid);
-    pid_set_gains(&nastya_cs.vy_pid, 1, 0, 0); // KP, KI, KD
+    pid_set_gains(&nastya_cs.vy_pid, 20, 4, 1); // KP, KI, KD
     pid_set_maximums(&nastya_cs.vy_pid, VY_MAX_INPUT, 1024, 0); // in , integral, out
     pid_set_out_shift(&nastya_cs.vy_pid, 0);
     pid_set_derivate_filter(&nastya_cs.vy_pid, 3);
     // angular velocity omega
     pid_init(&nastya_cs.omega_pid);
-    pid_set_gains(&nastya_cs.omega_pid, 0, 0, 0); // KP, KI, KD
+    pid_set_gains(&nastya_cs.omega_pid, 15, 2, 1); // KP, KI, KD
     pid_set_maximums(&nastya_cs.omega_pid, OMEGA_MAX_INPUT, 1024, 0); // in , integral, out
     pid_set_out_shift(&nastya_cs.omega_pid, 0);
     pid_set_derivate_filter(&nastya_cs.omega_pid, 3);
