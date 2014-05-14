60de7238ccef7a05795fb77dc4a8032d0155f4aa
Florian Reinhard <florian.reinhard@epfl.ch>
2014-05-14 20:00:03 +0200

    Merged obstacle_avoidance_protocol aka. Pius-protocol form debra's repo.

---
+++ obstacle_avoidance_protocol.c
@@ -0,0 +1,142 @@
+#include <stdlib.h>
+#include <string.h>
+#include <lwip/tcpip.h>
+#include <lwip/ip.h>
+#include <lwip/sys.h>
+#include "obstacle_avoidance_protocol.h"
+#include "json.h"
+
+
+void obstacle_avoidance_request_create(obstacle_avoidance_request_t *r, int obstacle_count)
+{
+    memset(r, 0, sizeof(obstacle_avoidance_request_t));
+    r->obstacles = malloc(sizeof(obstacle_avoidance_obstacle_t) * obstacle_count);
+    r->obstacle_count = obstacle_count;
+}
+
+void obstacle_avoidance_request_delete(obstacle_avoidance_request_t *r)
+{
+    free(r->obstacles);
+    r->obstacles = NULL;
+    r->obstacle_count = 0;
+}
+
+char *obstacle_avoidance_request_encode(obstacle_avoidance_request_t *r)
+{
+    JsonNode *node;
+    JsonNode *array_head;
+    JsonNode *obstacle_head;
+    char *ret;
+    int i;
+    node = json_mkarray();
+
+    array_head = json_mkarray();
+
+    /* Start coordinates */
+    json_append_element(array_head, json_mknumber(r->start.x));
+    json_append_element(array_head, json_mknumber(r->start.y));
+    json_append_element(array_head, json_mknumber(r->start.vx));
+    json_append_element(array_head, json_mknumber(r->start.vy));
+    json_append_element(node, array_head);
+
+    json_append_element(node, json_mknumber(r->desired_samplerate));
+    json_append_element(node, json_mknumber(r->desired_datapoints));
+
+    array_head = json_mkarray();
+    for (i=0;i<r->obstacle_count;i++) {
+        obstacle_head = json_mkarray();
+        json_append_element(obstacle_head, json_mknumber(r->obstacles[i].x));
+        json_append_element(obstacle_head, json_mknumber(r->obstacles[i].y));
+        json_append_element(obstacle_head, json_mknumber(r->obstacles[i].vx));
+        json_append_element(obstacle_head, json_mknumber(r->obstacles[i].vy));
+        json_append_element(obstacle_head, json_mknumber(r->obstacles[i].r));
+        json_append_element(array_head, obstacle_head);
+    }
+
+    json_append_element(node, array_head);
+
+    ret = json_encode(node);
+    json_delete(node);
+    return ret;
+}
+
+int obstacle_avoidance_decode_path(obstacle_avoidance_path_t *path,const char *json)
+{
+    JsonNode *node, *n;
+    int i;
+
+    node = json_decode(json);
+
+    if (node == NULL || node->tag != JSON_ARRAY)
+        return ERR_VAL;
+
+    path->len = json_get_length(node);
+
+    path->points = malloc(sizeof(obstacle_avoidance_point_t) * path->len);
+
+    i = 0;
+    json_foreach(n, node) {
+        path->points[i].x  = json_find_element(n, 0)->number_;
+        path->points[i].y  = json_find_element(n, 1)->number_;
+        path->points[i].vx = json_find_element(n, 2)->number_;
+        path->points[i].vy = json_find_element(n, 3)->number_;
+        i++;
+    }
+
+    return ERR_OK;
+}
+
+int obstacle_avoidance_send_request(obstacle_avoidance_request_t *request, struct ip_addr remote_ip, int port, obstacle_avoidance_path_t *path)
+{
+    /* XXX find a better way to do this. */
+    #define MAX_LEN 2048
+    char *data, to_append[TCP_MSS+1];
+    char answer[MAX_LEN];
+    void *tmp;
+    struct netconn *conn = NULL;
+
+    struct netbuf *buf;
+    u16_t len;
+    int err;
+
+    data = obstacle_avoidance_request_encode(request);
+
+    conn = netconn_new(NETCONN_TCP);
+
+    err = netconn_connect(conn, &remote_ip, 2048);
+    if (err != ERR_OK) {
+        free(data);
+        return err;
+    }
+
+    err = netconn_write(conn, data, strlen(data), NETCONN_COPY);
+
+    if (err != ERR_OK) {
+        free(data);
+        return err;
+    }
+
+    free(data);
+
+    while ((err = netconn_recv(conn, &buf)) == ERR_OK) {
+        do {
+            /* Copies the buffer data into a string. */
+            netbuf_data(buf, &tmp, &len);
+            memcpy(to_append, tmp, len);
+            to_append[len] = 0;
+            snprintf(answer, MAX_LEN, "%s%s", answer, to_append);
+        } while (netbuf_next(buf) >= 0);
+        netbuf_delete(buf);
+    }
+
+    err = obstacle_avoidance_decode_path(path, answer);
+    netconn_delete(conn);
+    return err;
+}
+
+void obstacle_avoidance_delete_path(obstacle_avoidance_path_t *path)
+{
+    path->points = NULL;
+    path->len = 0;
+    free(path->points);
+}
---
+++ obstacle_avoidance_protocol.h
@@ -0,0 +1,45 @@
+#ifndef OBSTACLE_AVOIDANCE_H_
+#define OBSTACLE_AVOIDANCE_H_
+
+#include <lwip/ip.h>
+
+typedef struct {
+    int x,y; /* mm */
+    int vx, vy; /* mm / s */
+    int r; /* mm */
+} obstacle_avoidance_obstacle_t;
+
+typedef struct {
+    int vx, vy;
+    int x, y;
+    int timestamp;
+} obstacle_avoidance_point_t;
+
+typedef struct {
+    obstacle_avoidance_point_t *points;
+    int len;
+} obstacle_avoidance_path_t;
+
+typedef struct {
+    int desired_datapoints;
+    int desired_samplerate; /* ms */
+
+    obstacle_avoidance_point_t start;
+
+    obstacle_avoidance_obstacle_t *obstacles;
+    int obstacle_count;
+} obstacle_avoidance_request_t;
+
+
+void obstacle_avoidance_request_create(obstacle_avoidance_request_t *r, int obstacle_count);
+
+void obstacle_avoidance_request_delete(obstacle_avoidance_request_t *r);
+
+char *obstacle_avoidance_request_encode(obstacle_avoidance_request_t *r);
+
+int obstacle_avoidance_decode_path(obstacle_avoidance_path_t *path,const char *json);
+
+void obstacle_avoidance_delete_path(obstacle_avoidance_path_t *path);
+
+int obstacle_avoidance_send_request(obstacle_avoidance_request_t *request, struct ip_addr remote_ip, int port, obstacle_avoidance_path_t *path);
+#endif
