1636ff2f09640cd748c0e4eab650f4223dc10c99
Florian Reinhard <florian.reinhard@epfl.ch>
2014-05-17 19:21:03 +0200

    minor bug fixes

--- drive_waypoint.c
+++ drive_waypoint.c
@@ -41,6 +41,11 @@ void drive_waypoint_init()
     destination.vx = 0;
     destination.vy = 0;
 
+    path.points = NULL;
+    path.len = 0;
+
+    request_time = uptime_get();
+
     param_add(&drive_request_timeout, "drive_request_timeout", "[us]");
     param_add(&desired_nb_datapoints, "desired_nb_datapoints", NULL);
     param_add(&desired_sample_period, "desired_sample_period", "[ms]");
@@ -79,6 +84,12 @@ drive_waypoint_t* drive_waypoint_get_next()
     INT8U uCErr;
     OSSemPend(mutex, 0, &uCErr);
 
+    int32_t relative_now = uptime_get() - request_time;
+    if (relative_now > param_get(&drive_request_timeout)) {
+        OSSemPost(mutex);
+        return NULL;
+    }
+
     if (path.len == 0) {
         OSSemPost(mutex);
         LOCK();
@@ -88,12 +99,6 @@ drive_waypoint_t* drive_waypoint_get_next()
         next_waypoint.vx = 0;
         next_waypoint.vy = 0;
         return &next_waypoint;
-    }
-
-    int32_t relative_now = uptime_get() - request_time;
-    if (relative_now > param_get(&drive_request_timeout)) {
-        OSSemPost(mutex);
-        return NULL;
     }
 
     int i;
