dacb62c7fb273d70be8b7cc0dd734f1820a2607e
Patrick Spieler <stapelzeiger@gmail.com>
2014-05-23 15:20:09 +0200

    net trigger multiple times

--- match.c
+++ match.c
@@ -209,8 +209,25 @@ end_of_match:
 
     // fire
     hw_set_net(1);
-    OSTimeDly(OS_TICKS_PER_SEC * 8);
+    OSTimeDly(OS_TICKS_PER_SEC/2);
     hw_set_net(0);
+    OSTimeDly(OS_TICKS_PER_SEC/2);
+    hw_set_net(1);
+    OSTimeDly(OS_TICKS_PER_SEC/2);
+    hw_set_net(0);
+    OSTimeDly(OS_TICKS_PER_SEC/2);
+    hw_set_net(1);
+    OSTimeDly(OS_TICKS_PER_SEC/2);
+    hw_set_net(0);
+    OSTimeDly(OS_TICKS_PER_SEC/2);
+    hw_set_net(1);
+    OSTimeDly(OS_TICKS_PER_SEC/2);
+    hw_set_net(0);
+    OSTimeDly(OS_TICKS_PER_SEC/2);
+    hw_set_net(1);
+    OSTimeDly(OS_TICKS_PER_SEC/2);
+    hw_set_net(0);
+    OSTimeDly(OS_TICKS_PER_SEC/2);
 
     printf("captured mammoth\n");
 
